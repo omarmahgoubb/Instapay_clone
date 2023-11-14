9abc86ef8fc509af8a633f2c2be6b23f909ec26d
Kafrawyyy <112349160+Kafrawyyy@users.noreply.github.com>
2023-11-14 23:27:04 +0200

    changed main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
+        BankValidation n1 = new BankValidation();
+      //  n1.verifyBankNumber("1111");
+        //n1.verifyBankNumber("777777");
+        //n1.verifyBankMobileNumber("0103");
+
+       // n1.verificationOfBank("11111" , "01030");
+      //    n1.otpVerification();
+        WalletValidation v1 = new WalletValidation();
+      // boolean otpverify =  v1.mobileNumberVerify("fawry" , "1111");
+        //if (otpverify )
+          //  System.out.println("true");
+       // v1.otpVerification();
+       v1.verificationWallet("fawry" ,"2222");
     }
 }
 
