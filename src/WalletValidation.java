8056f39e3b2b50ceef2661a67329c76ce947339f
Kafrawyyy <112349160+Kafrawyyy@users.noreply.github.com>
2023-11-14 23:22:00 +0200

    added Vallidation

---
+++ src/WalletValidation.java
@@ -0,0 +1,120 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class WalletValidation extends   Validation{
+
+    private String[] vodafonemobilenumbers = { "01012345678" ,"01098765432"};
+    private String[] cibMobileNumbers = {"1234" , "56789"};
+    private String[] fawryMobileNumbers = {"1111","2222"} ;
+    private int[] otpNumbers = {123, 456, 789, 888, 111 , 222 , 333 , 444 } ;
+    public boolean vodafoneMobileNumberVerify (String mobileNum )
+    {
+        for (String num : vodafonemobilenumbers)
+        {
+            if (num.equals(mobileNum) )
+            {
+                System.out.println("");
+                System.out.println("mobile number is verified successfully in Vodafone ");
+                return true;
+            }
+        }
+        System.out.println("mobile number not verified in Vodafone");
+        return false ;
+    }
+    public boolean cibMobileNumberVerify (String mobileNum )
+    {
+        for (String num : cibMobileNumbers)
+        {
+            if (num.equals(mobileNum) )
+            {
+                System.out.println("");
+                System.out.println("mobile number is verified successfully in CIB ");
+                return true;
+            }
+        }
+        System.out.println("mobile number not verified");
+        return false ;
+    }
+
+    public boolean fawryMobileVerify (String mobileNum )
+    {
+        for (String num : fawryMobileNumbers)
+        {
+            if (num.equals(mobileNum) )
+            {
+                System.out.println("");
+                System.out.println("mobile number is verified successfully  in fawry ");
+                return true;
+            }
+        }
+        System.out.println("mobile number not verified");
+        return false ;
+    }
+
+    public boolean mobileNumberVerify(String type , String number)
+    {
+
+        if (type == "vodafone")
+        {
+             if (vodafoneMobileNumberVerify(number))
+                 return true;
+        }
+        else if (type == "cib")
+        {
+            if (cibMobileNumberVerify(number))
+                return true;
+
+        }
+        else if (type =="fawry")
+        {
+            if (fawryMobileVerify(number))
+                return true;
+        }
+
+            return false;
+
+    }
+
+    public boolean otpVerification()
+    {
+        int randomIndex = new Random().nextInt(otpNumbers.length);
+        int randomNum = otpNumbers[randomIndex];
+        System.out.println("OTP number is : " + randomNum);
+          Scanner scanner = new Scanner(System.in);
+        int testnumber;
+        System.out.println("Please enter the otp number");
+        testnumber =  scanner.nextInt();
+        scanner.close();
+
+        System.out.println("OTP number is : " + randomNum + " you entered number : " + testnumber );
+
+
+        if (testnumber == randomNum)
+        {
+            System.out.println("OTP verified Successfully ");
+            return true;
+        }
+        else
+        {
+            System.out.println("error in otp verification !!");
+            return false;
+        }
+
+    }
+
+    public boolean verificationWallet (String type , String MobileNum)
+    {
+       if (mobileNumberVerify(type , MobileNum) && otpVerification() )
+       {
+           System.out.println(" ");
+           System.out.println("Wallet account verified succefully !!");
+           System.out.println(" ");
+           return true;
+       }
+        else
+            return false;
+    }
+
+
+
+}
