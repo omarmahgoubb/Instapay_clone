21adc001fb770dfc84a8ccb4585013cb7192373a
NourhanD <nourhandarwish002@gmail.com>
2023-11-15 16:17:50 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in) ;
-        InstaPayAccount account = new InstaPayAccount() ;
+        //InstaPayAccount account = new InstaPayAccount() ;
         String command ;
-        System.out.println("Nourhan");
         System.out.println("sign up / sign in");
         command = scanner.nextLine().toLowerCase();
         if (command == "sign up")
